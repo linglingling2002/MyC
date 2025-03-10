a834b05e9935f415950b36313ff316ac0dcb7afc
chanyue <73543114+linglingling2002@users.noreply.github.com>
2025-03-10 09:34:07 +0000

    日语默写1.0

--- Word/japanese.cpp
+++ Word/japanese.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <fstream>
 #include <ctime>
 #include <random>
 #include <algorithm>
+#include <iomanip> // 包含 std::setw 和 std::left
 
+/** 
+ * 在同文件夹下的word.txt中输入。
+ * 支持“とざん 登山 登山”格式
+ * 也支持以下格式：
+ *     ぼうはん
+ *     防犯
+ *     防止犯罪
+ * 只要保证三个一组即可。
+ * 
+ *  本程序不把换行或空格作为单词, 有空则使用 “-” 或其他不影响阅读的符号占位即可。
+ */
 
 class Word{
 private:
@@ -14,10 +26,15 @@ private:
     std::string meaning;
 
 public:
+    Word(){
+        letter = "";
+        wordStage = "";
+        meaning = "";
+    }
     Word(std::string letter, std::string wordStage, std::string meaning) 
         : letter(letter), wordStage(wordStage), meaning(meaning){
-            ;
-        }
+        ;
+    }
     void setLetter(std::string letter){
         this->letter = letter;
     }
@@ -37,81 +54,90 @@ public:
         return this->meaning;
     }
     void printWord(){
-        std::cout << getLetter() << " | " << getWordStage() << " | " << getMeaning() << std::endl;
+        std::cout << std::left  // 左对齐
+        << std::setw(20) << getLetter() << " | " << std::setw(12) << getWordStage() << " | " << std::setw(30) << getMeaning() << std::endl;
     }
 };
 
 void line(){
-    for(int i = 0; i < 30; i++){
+    for(int i = 0; i < 15; i++){
         std::cout << std::endl;
     }
     std::cout << "-------------------------------------------" << std::endl;
-    for(int i = 0; i < 30; i++){
+    for(int i = 0; i < 15; i++){
         std::cout << std::endl;
     }
 }
 
 int main(){
     int mode = 0;
-    std::vector<Word> wordGroup = {{"けんとう", "見当", "估计"}, 
-                                    {"こきゅう", "呼吸", "呼吸；窍门"}, 
-                                    {"1", "設計", "设计"}, 
-                                    {"2", "呼吸", "呼吸；窍门"}, 
-                                    {"3", "設計", "设计"}, 
-                                    {"4", "呼吸", "呼吸；窍门"}, 
-                                    {"57", "設計", "设计"}, 
-                                    {"2", "呼吸", "呼吸；窍门"}, 
-                                    {"3", "設計", "设计"}, 
-                                    {"4", "呼吸", "呼吸；窍门"}, 
-                                    {"57", "設計", "设计"}, 
-                                    {"2", "呼吸", "呼吸；窍门"}, 
-                                    {"3", "設計", "设计"}, 
-                                    {"4", "呼吸", "呼吸；窍门"}, 
-                                    {"57", "設計", "设计"}, 
-                                    {"ぼしゅう", "募集", "募集"}};
-    // for(int i = 0; i < wordGroup.size(); i++){
-    //     wordGroup[i].printWord();
-    // }
+    int count = 0;
+    std::vector<Word> wordGroup;
+    char data[100];    
+    std::ifstream inFile;
+    inFile.open("word.txt");
 
-    std::vector<int> numbers;
-    for (int i = 2; i <= 20; i++) {
-        numbers.push_back(i);
+    while(!inFile.eof()){
+        Word newWord;
+        std::string newStr;
+
+        inFile >> data;
+        newStr = data;
+        newWord.setLetter(newStr);
+        inFile >> data;
+        newStr = data;
+        newWord.setWordStage(newStr);
+        inFile >> data;
+        newStr = data;
+        newWord.setMeaning(newStr);
+
+        wordGroup.push_back(newWord);
     }
-    
-    // 随机打乱顺序
-    std::default_random_engine e(time(0));
-    std::shuffle(numbers.begin(), numbers.end(), e);
-    
-    // 输出前10个数
-    // for (int i = 0; i < 10 && i < numbers.size(); i++) {
-    //     std::cout << numbers[i] << std::endl;
-    // }
 
     do{
         std::cout << "-------------------------------------------" << std::endl;
-        std::cout << "选择模式 输入1，看假名写汉字模式；输入2，看汉字写假名模式；输入3，浏览模式输入0，程序结束:";
+        std::cout << "选择模式 输入1, 看假名写汉字模式; 输入2, 看汉字写假名模式; 输入3, 浏览模式输入0, 程序结束:";
         std::cin >> mode;
-        if(1 == mode){
-            for(int i = 0; i < 10; i++){
-                std::cout << wordGroup[numbers[i]].getLetter() << " | " << wordGroup[numbers[i]].getWordStage() << std::endl;
-            }
-            line();
-            for(int i = 0; i < 10; i++){
-                std::cout << wordGroup[numbers[i]].getLetter() << std::endl;
-            }
-        }else if(2 == mode){
-            for(int i = 0; i < 10; i++){
-                std::cout << wordGroup[numbers[i]].getLetter() << " | " << wordGroup[numbers[i]].getWordStage() << std::endl;
-            }
-            line();
-            for(int i = 0; i < 10; i++){
-                std::cout << wordGroup[numbers[i]].getWordStage() << std::endl;
-            }
+
+        if(0 == mode){
+            break;
         }else if(3 == mode){
             for(int i = 0; i < wordGroup.size(); i++){
                 wordGroup[i].printWord();
             }
+        }else{
+            std::cout << "输入默写个数 (单个单元最好不超过20) :";
+            std::cin >> count;
+            std::cout << "-------------------------------------------" << std::endl;
+
+            std::vector<int> numbers;
+            for (int i = 1; i <= count; i++) {
+                numbers.push_back(i);
+            }
+            // 随机打乱顺序
+            std::default_random_engine e(time(0));
+            std::shuffle(numbers.begin(), numbers.end(), e);
+            std::cout << std::endl;
+    
+            if(1 == mode){
+                for(int i = 0; i < count; i++){
+                    std::cout << std::left << std::setw(20) << wordGroup[numbers[i]].getLetter() << " | " << std::setw(12) << wordGroup[numbers[i]].getWordStage() << std::endl;
+                }
+                line();
+                for(int i = 0; i < count; i++){
+                    std::cout << wordGroup[numbers[i]].getLetter() << std::endl;
+                }
+            }else if(2 == mode){
+                for(int i = 0; i < count; i++){
+                    std::cout << std::left << std::setw(20) << wordGroup[numbers[i]].getLetter() << " | " << std::setw(12) << wordGroup[numbers[i]].getWordStage() << std::endl;
+                }
+                line();
+                for(int i = 0; i < count; i++){
+                    std::cout << wordGroup[numbers[i]].getWordStage() << std::endl;
+                }
+            }
         }
+        
     }while(0 != mode);
 
     std::cout << "程序结束。" << std::endl;
